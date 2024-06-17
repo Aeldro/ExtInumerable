7cb1d2ff46fb47e2a0134a17fea005f391989990
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-17 11:56:42 +0200

    Ajoutez des fichiers projet.

---
+++ ExtInumerable/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExtInumerable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine("Enter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int addition = integers.Where(x => x <= number).Aggregate((x, y) => x + y);
+            int result = addition / integers.Where(x => x <= number).Count();
+            Console.WriteLine(result);
+        }
+    }
+}
